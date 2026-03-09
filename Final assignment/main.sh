b53cbba2c8603cd94fb8deda2148a65330f99e0f
Smit <n.m.g.smit@student.tue.nl>
2026-03-09 16:09:39 +0100

    first run main.sh job

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 100 \
+    --epochs 10 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet-training" \
+    --experiment-id "First_run" \
